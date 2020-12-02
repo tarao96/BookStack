762e4660df4415f9fae5cfbde68c6af020e21997
EC2 Default User <ec2-user@ip-172-31-46-144.us-east-2.compute.internal>
2020-12-02 11:19:49 +0000

    Add login logout user_new user_edit

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,4 @@
 class StaticPagesController < ApplicationController
-  before_action :forbid_login_user, {only: [:home]}
   
   def home
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
   
-  root 'static_pages#home'
-  get 'about', to:'static_pages#about'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/login', to: 'users#login_form'
+  post '/login', to: 'users#login'
+  post '/logout', to: 'users#logout'
+  post '/users/:id/update', to: 'users#update'
+  get '/users/:id/edit', to: 'users#edit'
+  post '/users/create', to: 'users#create'
+  get '/signup', to: 'users#new'
+  get '/users/index', to: 'users#index'
+  get '/users/:id', to: 'users#show'
+  
+  get '/posts/index', to: 'posts#index'
+  
+  get root to: 'static_pages#home'
+  get '/about', to: 'static_pages#about'
 end
